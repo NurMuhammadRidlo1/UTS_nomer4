96be492c50d5083f290f0cc7c0936e3bffb14b57
NurMuhammadRidlo1 <nurmuhammadridlo@gmail.com>
2022-05-24 01:31:45 +0700

    first commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleApp2;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            dosen obj = new dosen();
+            obj.Nik = 12345;
+            obj.gender = "laki-laki";
+            obj.attDosen("Nur", 1212);
+            obj.addCourse("Pemrograman");
+            obj.Dosen();
+        }
+    }
+}
---
+++ ConsoleApp2/dosen.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class dosen
+    {
+        public int id;
+        public string nama;
+        public int Nik;
+        public string gender;
+        public string course;
+        public void Dosen()
+        {
+            Console.WriteLine("Nik : " + Nik);
+            Console.WriteLine("Gender : " + gender);
+        }
+
+        public void attDosen(string nama, int id)
+        {
+            Console.WriteLine("Nama : " + nama);
+            Console.WriteLine("ID : " + id);
+        }
+
+        public void addCourse(string course)
+        {
+            Console.WriteLine("Course : " + course);
+        }
+
+
+    }
+}
